36c58f19ba62c7797c9f69f985e46149f1e55a8a
Deepank <deepankbajaj1999@gmail.com>
2021-01-05 23:25:29 +0530

    initial commit

---
+++ Lec1_JS_intro/Intro.js
@@ -0,0 +1,11 @@
+// top to down
+// left to right
+
+// Xmain function X, importX
+console.log("Hello world");
+
+//data types-> Number, String, undefined, boolean, object, null
+
+//ES6 Syntax => EcmaScript => let ya const variable declaration
+
+// let=> Block scoped variable
